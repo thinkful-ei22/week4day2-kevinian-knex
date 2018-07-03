bab5bc1f2ae2776355693f20f081f06519304220
Ian Beihl <ianbeihl@gmail.com>
2018-07-03 08:33:56 -0700

    first half of knex queries

--- queries.js
+++ queries.js
@@ -7,9 +7,48 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(JSON.stringify(results, null, 2)));
+
+// knex
+//   .select()
+//   .from('restaurants')
+//   .then(results => console.log(results));
+
+// knex
+//   .select()
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .limit(10)
+//   .then(results => console.log(results));
+
+// knex
+//   .select('id', 'name')
+//   .from('restaurants')
+//   .limit(10)
+//   .where('cuisine', 'Italian')
+//   .then(res => console.log(res));
+
+// knex
+//   .select()
+//   .count()
+//   .from('restaurants')
+//   .where('cuisine', 'Thai')
+//   .then(res => console.log(res));
+
+// knex
+//   .select()
+//   .count()
+//   .from('restaurants')
+//   .then(res => console.log(res));
+
 knex
   .select()
+  .count()
   .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(JSON.stringify(results, null, 2)));
+  .where({cuisine: 'Thai', address_zipcode: '11372'})
+  .then(res => console.log(res));
